1d79ebbd5d69b8631074626f10a8817755b362ca
CYBER DEXTER <152212325+darkewing@users.noreply.github.com>
2024-03-23 01:25:14 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -146,7 +146,7 @@ Secktor.cmd({
                     title: Config.ownername,
                     body: 'Touch here.',
                     renderLargerThumbnail: true,
-                    thumbnailUrl: `https://telegra.ph/file/457fe58703f09b5e7bf95.jpg`,
+                    thumbnailUrl: `https://telegra.ph/file/539756a96d644fa0fa343.jpg`,
                     thumbnail: log0,
                     mediaType: 2,
                     mediaUrl: '',
